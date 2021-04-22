7a2895739193a2ac218995faf29dbee443391919
lizus <lizus.com@gmail.com>
2021-04-22 16:40:55 +0800

    20210422

--- src/media/youku.php
+++ src/media/youku.php
@@ -3,5 +3,5 @@
 //
 //优酷
 wp_embed_register_handler( 'youku', '#https?://v.youku.com/v_show/id_(.*?).html#i', function ($matches,$attr,$url,$rawattr){
-	return '<div clss="media-wrap media-youku"><iframe height="100%" width="100%" src="//player.youku.com/embed/'.esc_attr($matches[1]).'" frameborder=0 allowfullscreen></iframe></div>';
+	return '<div class="media-wrap media-youku"><iframe height="100%" width="100%" src="//player.youku.com/embed/'.esc_attr($matches[1]).'" frameborder=0 allowfullscreen></iframe></div>';
 } );
